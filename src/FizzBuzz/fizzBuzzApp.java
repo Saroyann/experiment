b01fb6f7f4d1603b805a9baed3835514e8d90150
Yiim <ilyimyan@gmail.com>
2023-07-21 18:02:18 +0800

    initial commit

--- src/FizzBuzz/fizzBuzzApp.java
+++ src/FizzBuzz/fizzBuzzApp.java
@@ -1,4 +1,34 @@
 package FizzBuzz;
 
+import java.util.Scanner;
+
 public class fizzBuzzApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+
+        int n = sc.nextInt();
+        System.out.println("The fizz, buzz, and fizzbuzz number are : ");
+
+        for (int i = 1; i < n ; i++ ) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+
+            System.out.println(","+" ");
+
+            sc.close();
+        }
+
+    }
+
 }
