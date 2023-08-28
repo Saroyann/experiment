01729f95b7d00fc49884c0cf9373140361bfd999
Yiim <ilyimyan@gmail.com>
2023-08-28 21:12:36 +0800

    initial commit

--- src/factorial/factorial.java
+++ src/factorial/factorial.java
@@ -1,7 +1,9 @@
 package factorial;
 
+import java.util.Scanner;
+
 public class factorial {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
     }
 }
