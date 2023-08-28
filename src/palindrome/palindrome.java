7504a621718220091db294aeebd8834113727e12
Yiim <ilyimyan@gmail.com>
2023-08-28 19:11:07 +0800

    initial commit

--- src/palindrome/palindrome.java
+++ src/palindrome/palindrome.java
@@ -10,8 +10,15 @@ public class palindrome {
         while(n>0) {
             r=n%10;
             sum=(sum*10)+r;
-            n=n/10
+            n=n/10;
+        }
+        if(temp==sum) {
+            System.out.println("palindrome number");
+            else {
+                System.out.println("not palindrome");
+            }
         }
     }
+
 
 }
