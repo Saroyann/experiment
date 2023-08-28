2961b193006a8fd7a2f06f9ba00713eda46b4b06
Yiim <ilyimyan@gmail.com>
2023-08-28 21:14:12 +0800

    initial commit

--- src/factorial/factorial.java
+++ src/factorial/factorial.java
@@ -5,5 +5,9 @@ import java.util.Scanner;
 public class factorial {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.println("enter the number:");
+        int n = input.nextInt();
+        input.close();
     }
 }
