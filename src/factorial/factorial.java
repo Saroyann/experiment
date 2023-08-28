0cca92d15c045e6918170b6e9bf029bb22acd7a7
Yiim <ilyimyan@gmail.com>
2023-08-28 21:19:23 +0800

    initial commit

--- src/factorial/factorial.java
+++ src/factorial/factorial.java
@@ -11,5 +11,9 @@ public class factorial {
         input.close();
         int i,fact = 1;
 
+        for (i = 1 ; i <= number ; i++) {
+            fact = fact*i;
+        }
+        System.out.println("factorial of" + number + " is" + fact);
     }
 }
