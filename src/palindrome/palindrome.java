ef8ca1cb3fd6ea84440d55a7b09c49e0cd007b63
Yiim <ilyimyan@gmail.com>
2023-08-28 19:03:51 +0800

    initial commit

--- src/palindrome/palindrome.java
+++ src/palindrome/palindrome.java
@@ -3,7 +3,15 @@ package palindrome;
 public class palindrome {
 
     public static void main(String[] args) {
+        int r,sum = 0,temp;
+        int n = 454; // check the number palindrome
 
+        temp = n;
+        while(n>0) {
+            r=n%10;
+            sum=(sum*10)+r;
+            n=n/10
+        }
     }
 
 }
