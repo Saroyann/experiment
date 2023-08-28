8d759c811e32cf3e792f9f1132b3a07b190b539f
Yiim <ilyimyan@gmail.com>
2023-08-28 19:19:40 +0800

    initial commit

--- src/palindrome/palindrome.java
+++ src/palindrome/palindrome.java
@@ -1,9 +1,14 @@
 package palindrome;
 
+import java.util.Scanner;
+
 public class palindrome {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("enter the number palindrome:");
+    }
 
 
 }
